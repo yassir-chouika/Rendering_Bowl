25d5521eceab1bb2ea62f3256a541c67b38e0f8d
CHOUIKA Yassir <yassir.chouika@gmail.com>
2025-01-17 21:34:57 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        PaletteBlack: "#222831",
+        SlightlyBlack: "#393E46",
+        PaletteTeal: "#00ADB5",
+        PaletteGray: "#778DA9",
+      },
+      screens: {
+        tablet: "640px",
+        laptop: "1024px",
+        lgDesktop: "1530px",
+      },
+    },
+  },
+  plugins: [],
+};
